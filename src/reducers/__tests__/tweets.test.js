a3c3cd6e9c134d8150a277858fbc22510195c190
Letícia Costa <lcosta@verios.com.br>
2019-06-29 16:04:33 -0300

    adicionar testes do reducer de tweets

--- src/reducers/__tests__/tweets.test.js
+++ src/reducers/__tests__/tweets.test.js
@@ -1,5 +1,34 @@
 import reducer from './../tweets';
 
 describe('Reducer Tweets', () => {
-  
+  let state;
+
+  it('deve retornar o estado inicial', () => {
+    state = reducer(state, {});
+
+    expect(state).toEqual({
+      tweets: [],
+      tweetSelecionado: null,
+      mostraModal: false
+    });
+  });
+
+  it('deve atualizar a lista de tweets ao receber ATUALIZAR_TWEETS', () => {
+    state = reducer(state, {
+      type: 'ATUALIZAR_TWEETS',
+      payload: [
+        { conteudo: 'Um tweet', nomeUsuario: 'Felizberto' },
+        { conteudo: 'Outro tweet', nomeUsuario: 'Marquinhos' }
+      ]
+    });
+
+    expect(state).toEqual({
+      tweets: [
+        { conteudo: 'Um tweet', nomeUsuario: 'Felizberto' },
+        { conteudo: 'Outro tweet', nomeUsuario: 'Marquinhos' }
+      ],
+      tweetSelecionado: null,
+      mostraModal: false
+    })
+  })
 });

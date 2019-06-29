a107afd651444bdfb68e82ff076a1943bee3254a
Letícia Costa <lcosta@verios.com.br>
2019-06-29 11:40:34 -0300

    Adicionando config base do teste do tweet

---
+++ src/components/Tweet/index.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Tweet from './index';
+import { shallow } from 'enzyme';
+
+describe('Tweet', () => {
+  let props = {};
+  let tweet = null;
+  const mountTweet = function () {
+    if (!tweet) {
+      tweet = shallow(<Tweet {...props} />)
+    }
+
+    return tweet;
+  }
+
+  beforeEach(() => {
+    tweet = null;
+    props = {};
+  });
+
+  it('deve renderizar avatar url', () => {
+    props.avatarURL = '';
+    const tweet = mountTweet();
+  })
+})
